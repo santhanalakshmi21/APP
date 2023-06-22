b2f68d1e11f53964c344e5ecd14370801f248ec9
Santhanalakshmi <104186416+santhanalakshmi21@users.noreply.github.com>
2023-06-22 07:31:58 +0530

    Add files via upload

---
+++ Chat_Client_v1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 19:43:18 2020
+@author: Admin
+"""
+#!usr/bin/python
+import socket
+host = socket.gethostname()
+port = 12345
+s = socket.socket()		# TCP socket object
+s.connect((host,port))
+s.sendall(b'This will be sent to server')    # Send This message to server
+while True:
+    data = s.recv(1024)	    # Now, receive the echoed data from server
+    if data.decode() == 'quit' or not data:
+        print("Client Exiting")
+        break
+    else:
+        print('Server : '+data.decode()) # Print received(echoed) data
+        st = input('Client : ')
+        s.sendall(st.encode())
+s.close()				# close the connection
